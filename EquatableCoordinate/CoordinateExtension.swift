c1406131f081878731ef5096b80df820cbc47f3f
Eric Lilienstein <elilienstein@users.noreply.github.com>
2018-11-27 14:50:13 -0800

    initial commit

---
+++ EquatableCoordinate/CoordinateExtension.swift
@@ -0,0 +1,36 @@
+//
+//  CLCoordinateExtension.swift
+//  EquatableCoordinate
+//
+//  Created by Eric Lilienstein on 11/27/18.
+//  Copyright © 2018 Eric Lilienstein. All rights reserved.
+//
+// A utility for comparing two geo-coordinates
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocationCoordinate2D: Equatable {}
+
+public func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+    return lhs.latitude == rhs.latitude
+        && lhs.longitude == rhs.longitude
+}
+
+
+public func isSameSpot(_ lhs: CLLocationCoordinate2D, _ rhs: CLLocationCoordinate2D)->Bool {
+    
+    return lhs.latitude.truncate(places: 5) == rhs.latitude.truncate(places: 5)
+    && lhs.longitude.truncate(places: 5) == rhs.longitude.truncate(places: 5)
+}
+
+// an extension to type Double lets us compare latitude and longitude with predictable lengths
+
+extension Double
+{
+    func truncate(places : Int)-> Double
+    {
+        return Double(floor(pow(10.0, Double(places)) * self)/pow(10.0, Double(places)))
+    }
+}
---
+++ EquatableCoordinate/README.md
@@ -0,0 +1,23 @@
+#  Equatable conformance for CLLocationCoordinate2D
+
+# Includes: 
+- an extension to type CLLocationCoordinate2D to conform to Equatable
+- an extension to type Double that truncates a geo-coordinate with multiple decimal places.
+- a public function accepting 2 coordinates and returning a boolean value
+
+# Reasoning: 
+ If two coordinates are queried for approximate equality, their latitude and longitude need to be of predictable length. A value in decimal degrees to 5 decimal places is precise to 1.1132 meter at the equator. 
+
+# Use Cases: 
+Client performs a zone check-in. 
+Send the client custom information based on their location.
+
+
+# Exercise with isSameSpot(::)
+
+let coord1: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 37.358028, longitude: -121.9074856)
+let coord2: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 37.358029, longitude: -121.9074857)
+
+let result = isSameSpot(coord1, coord2) // returns true
+
+
